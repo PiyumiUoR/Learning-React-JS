3debcccd1f9f84a01c3e78ce64ce5023144fa8ba
PiyumiUoR <malkisarapiyumi918@gmail.com>
2024-06-26 19:28:03 +0300

    ex 4 & 5

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-if (true){
-    let name = 'Piyumi';
-    // alert(name);
-}
+// //exercise 1
 
-alert(name);
+// if (true){
+//     let name = 'Piyumi';
+//     // alert(name);
+// }
+
+// alert(name);
+
+// //exercise 2
+
+// let fname = 'Piyumi';
+// let lname = 'W';
+// let age = prompt("Guess Piyumi's age..");
+
+// // //old way
+// // let result = fname + ' ' + lname + ' '+ 'is ' + age + ' ' + 'years old.';
+// // alert(result);
+
+// //template strings
+// let result = `${fname} ${lname} is ${age} years old.`;
+// alert(result);
+
+// function welcome(user, message){
+//     alert(`Hello ${user}, ${message}`)
+// }
+
+// // welcome('Piyumi', 'Welcome!')
+// welcome();
+
+function welcome(user="mystery user", message="Good bye!"){
+    alert(`Hello ${user}, ${message}`)
+}
+// welcome('Piyumi', 'Welcome!')
+welcome();
